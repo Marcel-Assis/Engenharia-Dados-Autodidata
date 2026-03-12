0cd60d1931f61061db8b30113df0adf399c90de3
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-12 01:04:09 -0300

    m7

--- Python/7-Gestao de Modulos/3-criando-modulo.py
+++ Python/7-Gestao de Modulos/3-criando-modulo.py
@@ -1,2 +1,14 @@
 import teste
 print(dir(teste))
+print(teste.PI)
+var = teste.Teste()
+teste.MyFunc(10)
+
+from teste import PI
+print(PI)
+
+from teste import PI as NUMERO_PI
+print(NUMERO_PI)
+
+from teste import MyFunc
+MyFunc("Teste")
