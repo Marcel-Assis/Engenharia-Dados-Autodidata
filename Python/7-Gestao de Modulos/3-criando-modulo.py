e1c2c89011944f870e9e016344d0b7cc0672cd57
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-12 01:07:44 -0300

    m7

--- Python/7-Gestao de Modulos/3-criando-modulo.py
+++ Python/7-Gestao de Modulos/3-criando-modulo.py
@@ -1,14 +1,14 @@
-import teste
-print(dir(teste))
-print(teste.PI)
-var = teste.Teste()
-teste.MyFunc(10)
+import teste # Importação do módulo teste.py
+print(dir(teste)) # Exibição dos atributos e métodos do módulo
+print(teste.PI) # Acesso à constante PI do módulo
+var = teste.Teste() # Criação de uma instância da classe Teste
+teste.MyFunc(10) # Chamada da função MyFunc do módulo
 
-from teste import PI
-print(PI)
+from teste import PI # Importação da constante PI do módulo
+print(PI) # Exibição da constante PI
 
-from teste import PI as NUMERO_PI
-print(NUMERO_PI)
+from teste import PI as NUMERO_PI # Importação da constante PI com alias
+print(NUMERO_PI) # Exibição da constante PI com alias
 
-from teste import MyFunc
-MyFunc("Teste")
+from teste import MyFunc # Importação da função do módulo
+MyFunc("Teste") # Chamada da função MyFunc do módulo
