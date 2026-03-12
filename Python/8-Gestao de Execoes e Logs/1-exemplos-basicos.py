4f5f55c8946d4619e40be11c915e053889366d73
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-12 03:55:30 -0300

    m8

---
+++ Python/8-Gestao de Execoes e Logs/1-exemplos-basicos.py
@@ -0,0 +1,11 @@
+print('Inicio')
+lista = [1,2,3]
+try: # Código que pode dar erro
+    print(f'Try: {lista[10]}')
+except Exception as error: # Código executado se der erro
+    print(f"Except: Ocorreu um erro: {error}")
+else: # Código executado se não der erro
+    print("Else: Nenhum erro ocorreu.")
+finally: # Código executado sempre
+    print("Finally: Fim do bloco try-except")
+print('Fim')
