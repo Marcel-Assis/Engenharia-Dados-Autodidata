76881196b6cb94eb8f8906c94e27262d00f905c9
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-12 02:12:15 -0300

    m7

---
+++ Python/7-Gestao de Modulos/5-criando-lendo-ajuda.py
@@ -0,0 +1,9 @@
+import sys
+print(sys.__doc__) # Exibe a documentação do módulo sys
+
+print(print.__doc__) # Exibe a documentação da função print
+
+import teste
+print(teste.__doc__) # Exibe a documentação do módulo teste
+print(teste.soma.__doc__) # Exibe a documentação da função soma do módulo teste
+print(teste.Teste.__doc__) # Exibe a documentação da classe Teste do módulo teste
--- Python/7-Gestao de Modulos/teste.py
+++ Python/7-Gestao de Modulos/teste.py
@@ -1,8 +1,23 @@
+'''
+Este é o arquivo principal contendo uma variável chamada PI, uma função chamada soma e uma classe chamada Teste
+'''
+
 PI = 3.1415
 
 class Teste:
+    '''Classe de teste'''
     def __init__(self):
         print('Classe teste')
 
 def MyFunc(num):
+    '''Função de teste que imprime o número recebido como parâmetro'''
     print(num)
+
+def soma(num1, num2):
+    '''Função que retorna a soma de dois números recebidos como parâmetros'''
+    return num1 + num2
+
+print(__doc__) # Exibe a documentação do módulo
+print(soma.__doc__) # Exibe a documentação da função soma
+
+#help(soma) # Exibe a ajuda interativa da função soma
