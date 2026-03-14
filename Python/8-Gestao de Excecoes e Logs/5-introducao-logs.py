e5bb4aee4eb5b60bd81f518520122f5c1cdd8d09
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-13 23:59:26 -0300

    m8

---
+++ Python/8-Gestao de Excecoes e Logs/5-introducao-logs.py
@@ -0,0 +1,12 @@
+# Logging
+# O módulo logging é uma ferramenta poderosa para registrar eventos e mensagens em um programa Python. Ele permite que os desenvolvedores criem logs de diferentes níveis de severidade, como DEBUG, INFO, WARNING, ERROR e CRITICAL. Esses logs podem ser configurados para serem exibidos no console, gravados em arquivos ou enviados para outros destinos.
+# O logging é útil para depuração, monitoramento e análise de aplicativos, ajudando a identificar problemas, entender o fluxo do programa e registrar informações importantes durante a execução. Ele é amplamente utilizado em projetos de software para melhorar a manutenção e a resolução de problemas, fornecendo insights valiosos sobre o comportamento do aplicativo.
+import logging
+# Configuração básica do logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# Exemplo de uso do logging
+logging.debug("Esta é uma mensagem de depuração.")
+logging.info("Esta é uma mensagem informativa.")
+logging.warning("Esta é uma mensagem de aviso.")
+logging.error("Esta é uma mensagem de erro.")
+logging.critical("Esta é uma mensagem crítica.")
