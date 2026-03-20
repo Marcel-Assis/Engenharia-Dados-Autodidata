34ea45e9deae78542ea3ecd78ec588255a252283
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-20 02:23:00 -0300

    m10

---
+++ Python/10-Arquivos/3-lendo-arquivos.py
@@ -0,0 +1,4 @@
+arquivo = open("exemplo2_with.txt", "rt") # Abrir o arquivo para leitura
+conteudo = arquivo.read() # Ler o conteúdo do arquivo
+print(conteudo) # Imprimir o conteúdo do arquivo
+arquivo.close() # Fechar o arquivo
