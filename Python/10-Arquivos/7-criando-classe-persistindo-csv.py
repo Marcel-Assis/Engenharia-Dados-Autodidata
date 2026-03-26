2ce96b0c3c8ad4a82c4d718d2acb292e94579499
Marcel Assis <marcel.hrzn@gmail.com>
2026-03-25 23:56:04 -0300

    m10

--- Python/10-Arquivos/7-criando-classe-persistindo-csv.py
+++ Python/10-Arquivos/7-criando-classe-persistindo-csv.py
@@ -1,3 +1,4 @@
+import csv
 class Pessoa:
     def __init__(self, id, nome, profissao):
         self.id = id
@@ -12,3 +13,20 @@ class Pessoa:
                 pessoa = Pessoa(linha[0], linha[1], linha[2])
                 pessoas.append(pessoa)
             return pessoas
+    @staticmethod
+    def salva_pessoas(*pessoas):
+        with open("pessoas.csv", "w") as arquivo:
+            escritor = csv.writer(arquivo, delimiter=",")
+            for pessoa in pessoas:
+                escritor.writerow([pessoa.id, pessoa.nome, pessoa.profissao])
+            print("Pessoas salvas com sucesso.")
+        
+pessoa1 = Pessoa(1, "João", "Engenheiro")
+pessoa2 = Pessoa(2, "Maria", "Médica")
+pessoa3 = Pessoa(3, "Pedro", "Professor")
+
+Pessoa.salva_pessoas(pessoa1, pessoa2, pessoa3)
+
+lista_pessoa = Pessoa.le_pessoas()
+for pessoa in lista_pessoa:
+    print(f"ID: {pessoa.id}, Nome: {pessoa.nome}, Profissão: {pessoa.profissao}")
