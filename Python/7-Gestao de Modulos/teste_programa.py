cc1ac3771f68ad8893d823cadb1092fbe3c36def
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-03 18:39:58 -0300

    voltando aos estudos

--- Python/7-Gestao de Modulos/teste_programa.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if (__name__ == '__main__'):
-    if len(sys.argv) != 3:
-        print('Números de argumentos incorretos')
-        sys.exit(1)
-    numero1 = float(sys.argv[1])
-    numero2 = float(sys.argv[2])
-    print(numero1 + numero2)
