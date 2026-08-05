8a89b21a43d20ddf370bb38d7a0cab606b4d451e
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-05 17:32:28 -0300

    3-modulos

---
+++ Python/7-Gestao de Modulos/3-pip.py
@@ -0,0 +1,5 @@
+# é usado, através do terminal, para instalar pacotes externos no computador ou em um ambiente virtual
+# pip list -> lista todos os pacotes instalados
+# pip install <nome do pacote> -> instala pacotes ex: pip install pandas
+# pip uninstall <nome do pacote> -> desinstala um pacote
+# pip install <nome do pacote>==1.1.1 -> instala uma versão específica
