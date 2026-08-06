6446c30f23c4dd235ed524619bc0fd2b2a53e738
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-06 15:31:03 -0300

    6-logs

---
+++ Python/8-Gestao de Excecoes e Logs/6-logs.py
@@ -0,0 +1,30 @@
+# Introdução a Logs
+
+# Registro da Execução do Programa:
+#   Erros
+#   Etapas cumpridas
+#   Avisos
+
+# Por que?
+#   Informação
+#   Depuração
+#   Registro de Atividade para fins de Auditoria
+
+# Onde?
+#   Tela
+#   Arquivo
+#   Banco de Dados
+
+# ---
+
+# Python: logging
+    # Classe de Gestão de Logs
+    # Permite salvar em disco
+    # Formatar saída
+
+    # Levels: (Eu defino qual o nível hierárquico do log)
+        # Debug
+        # Info
+        # Warning
+        # Error
+        # Critical
