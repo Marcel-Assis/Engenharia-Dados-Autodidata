0d11bf6f174c419572b7bc5483feb518522eea46
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-09 13:35:53 -0300

    lendo arquivos

---
+++ Python/10-Arquivos/1-lendo_arquivos.py
@@ -0,0 +1,15 @@
+# Lendo Arquivos
+
+# Open
+# Primeira Parte
+# r abre um arquivo pra leitura, ele precisa existir, se não gera exceção
+# a abre um arquivo para anexar o que quiser nele, cria se não existir
+# w abre um arquivo para escrita, se não existir, cria
+# x cria um arquivo, mas se ele já existir retorna erro
+
+# Segunda parte
+# t abre um arquivo em modo texto
+# b abre um arquivo em modo binário
+
+# Padrão é rt (abre pra leitura em modo texto)
+
