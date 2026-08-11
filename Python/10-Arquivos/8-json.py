a88af3ef04174f9745e19980cdc98411f4e3cf12
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-11 18:47:31 -0300

    formato json

---
+++ Python/10-Arquivos/8-json.py
@@ -0,0 +1,20 @@
+# Formato Json
+
+# É um formato de arquivo que a estrutura é semelhante à estrutura do dicionário em python
+
+# Exemplo
+
+formato_json = {
+    'Rodrigo': {
+        "cpf": "12345",
+        "sexo": "masculino",
+        'Endereço': 'rua x',
+        'idade': 32
+    },
+    'Fernanda': {
+        "cpf": "54321",
+        "sexo": "feminino",
+        'Endereço': 'rua y',
+        'idade': 23
+    }
+}
