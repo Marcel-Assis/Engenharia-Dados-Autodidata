5f4e53e8208cfe34333087bd11b8bf0fa7d898c6
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-12 08:04:56 -0300

    # Formato XML

---
+++ Python/10-Arquivos/11-xml.py
@@ -0,0 +1,11 @@
+# Formato XML
+
+# é um tipo de arquivo em que as informações são estruturadas parecido com HTML, usando tags que possuem hierarquia
+
+# <DadosPessoas>
+#     <Pessoa nome="Rodrigo">
+#         <CPF>123456</CPF>
+#         <Sexo>Masculino</Sexo>
+#         <Endereco>Casa x</Endereco>
+#     </Pessoa>
+# </DadosPessoas>
