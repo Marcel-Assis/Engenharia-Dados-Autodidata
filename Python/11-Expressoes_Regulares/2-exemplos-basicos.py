4cea0a5d6396984e315d53b1ff8b1b171b76e5f0
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-15 23:28:50 -0300

    # Exemplos Básicos

---
+++ Python/11-Expressoes_Regulares/2-exemplos-basicos.py
@@ -0,0 +1,23 @@
+# Exemplos Básicos
+
+import re # importa o regex
+
+texto = '001234510'
+info = re.search('1', texto) # define o que quer encontrar e atribui na variável info
+if info != None: # se não houver, retorna none, se houver, segue
+    print('Encontrado ocorrência em', info.span()) # exibe em qual índice foi encontrado
+    print('O que foi encontrado:', info.group()) # exibe o que foi encontrado
+else:
+    print('Nada foi encontrado')
+
+# encontrar todas as ocorrências
+info2 = re.findall('1', texto) # findall retorna uma lista com todas as ocorrencias
+print(info2)
+
+# dividir a string e transformar em lista o que não for pedido
+info3 = re.split('1', texto) # o split divide a string antes e depois do encontrado, sem mostrar o que foi pedido
+print(info3)
+
+# substituir um caractere por outro
+info4 = re.sub('1', '#', texto) # sub substitui o 1 por #
+print(info4)
