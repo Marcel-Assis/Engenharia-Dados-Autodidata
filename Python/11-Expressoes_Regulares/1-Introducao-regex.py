ca75498a0bda920351bb9cc49b6d78e5dd45385c
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-15 23:10:15 -0300

    # Introdução a Expressões Regulares

---
+++ Python/11-Expressoes_Regulares/1-Introducao-regex.py
@@ -0,0 +1,7 @@
+# Introdução a Expressões Regulares
+
+# São utilizadas pra verificar se um conjunto de caracteres atende um determinado padrão
+# Se uma senha possui determinados caracteres obrigatórios, se um dado é válido: e-mail, data, hora, etc
+# Se um texto possui uma determinada letra ou palavra
+# Extrair determinadas partes de um texto, ex: CEP
+# Substituir caracteres (% por %% em URLS)
