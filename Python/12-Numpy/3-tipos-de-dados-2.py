bbb12ccd3a58e13aa9fe8eba0d04e3878cabb870
Marcel Assis <marcel.hrzn@gmail.com>
2026-08-16 14:47:04 -0300

    # Tipos de Dados II

---
+++ Python/12-Numpy/3-tipos-de-dados-2.py
@@ -0,0 +1,25 @@
+# Tipos de Dados II
+
+import numpy
+
+#64 bits
+ponto_flutuante = numpy.float64(10564.34)
+print(ponto_flutuante, type(ponto_flutuante))
+#32 bits
+ponto_flutuante2 = numpy.float32(39994.34)
+print(ponto_flutuante2, type(ponto_flutuante2))
+#8 bits
+int8 = numpy.int8(20)
+print(int8, type(int8))
+#16 bits
+int16 = numpy.int16(1000)
+print(int16, type(int16))
+#sem sinal 8 bits
+uint8 = numpy.uint8(34)
+print(uint8, type(uint8))
+#sem sinal 16 bits
+uint16 = numpy.uint16(344)
+print(uint16, type(uint16))
+#ponto flutuante 16 bits
+float16 = numpy.float16(16)
+print(float16, type(float16))
